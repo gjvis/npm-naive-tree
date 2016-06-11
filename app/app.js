92615394c89102664ef3eeaee250ec8aba971236
Gareth Visagie <gareth@gjvis.com>
2016-06-11 21:49:35 +0100

    Log to file instead of STDOUT when running tests

--- app/app.js
+++ app/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const errorHandlers = require('./error-handlers');
+const fs = require('fs');
 const logger = require('morgan');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
 
-app.use(logger('dev'));
+// Do not log to stdout when testing
+if (app.get('env') === 'test') {
+  const logStream = fs.createWriteStream(
+    path.join(__dirname, '..', 'logs', 'test.log'), { flags: 'w' });
+  app.use(logger('tiny', { stream: logStream }));
+} else {
+  app.use(logger('dev'));
+}
+
 app.use('/', routes);
 
 errorHandlers.init(app);
