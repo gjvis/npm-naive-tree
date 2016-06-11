f535e820b43d71630e9790fbbdf1ecb9cf2f71ce
Gareth Visagie <gareth@gjvis.com>
2016-06-11 14:11:51 +0100

    Add top-level acceptance tests (all pending)

--- test/acceptance.js
+++ test/acceptance.js
@@ -1,3 +1,64 @@
 describe('acceptance', () => {
+  context(`Given the following module structure:
+      A
+      ├── B
+      ├─┬ C
+      │ ├── F
+      │ └── G
+      ├─┬ D
+      │ ├─┬ H
+      │ │ ├── F
+      │ │ └── G
+      │ └─┬ I
+      │   ├── J
+      │   └── K
+      └─┬ E
+        └─┬ I
+          ├── J
+          └── K
+  `, () => {
 
-})
+    describe('GET /B (a module with no dependencies)', () => {
+      it('should resolve a tree containing only B');
+    });
+
+    describe('GET /C (a module with only immediate dependencies)', () => {
+      it(`should resolve:
+            C
+            ├── F
+            └── G`);
+    });
+
+    describe('GET /D (a module with multiple levels of dependencies)', () => {
+      it(`should resolve:
+            D
+            ├─┬ H
+            │ ├── F
+            │ └── G
+            └─┬ I
+              ├── J
+              └── K`);
+    });
+
+    describe('GET /A (a module with multiple levels of common dependencies)', () => {
+      it(`should resolve:
+            A
+            ├── B
+            ├─┬ C
+            │ ├── F
+            │ └── G
+            ├─┬ D
+            │ ├─┬ H
+            │ │ ├── F
+            │ │ └── G
+            │ └─┬ I
+            │   ├── J
+            │   └── K
+            └─┬ E
+              └─┬ I
+                ├── J
+                └── K`);
+    });
+
+  });
+});
