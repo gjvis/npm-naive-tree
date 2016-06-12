fa81f29d40111b3e04696cbdfc6bbce039b8bd00
Gareth Visagie <gareth@gjvis.com>
2016-06-12 20:53:13 +0100

    Move express-generated error handlers to their own file

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const errorHandlers = require('./error-handlers');
 const logger = require('morgan');
 const routes = require('./routes/index');
 
@@ -7,34 +8,6 @@ const app = express();
 app.use(logger('dev'));
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err.stack,
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-  });
-});
+errorHandlers.init(app);
 
 module.exports = app;
